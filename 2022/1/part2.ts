ba9e35c8a6aa8abe68b3713057a31a6d515c139a
Fayne Aldan <FayneAldan@gmail.com>
2022-12-04 23:11:43 -0700

    Day 1
    I'm late sorry ;w;

---
+++ 2022/1/part2.ts
@@ -0,0 +1,10 @@
+const input = Deno.readTextFileSync(
+  new URL("input.txt", import.meta.url)
+).replace(/\r/g, "");
+
+const array = input //
+  .split("\n\n")
+  .map((v) => v.split("\n").map(Number))
+  .map((v) => v.reduce((sum, n) => sum + n, 0))
+  .sort((a, b) => b - a);
+console.log(array[0] + array[1] + array[2]);
